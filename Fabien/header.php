59428ff5d3b11319eb27623433a22dfb2791c743
Fabien Turgut <Fabien Turgut>
2016-11-24 14:46:22 +0100

    fabien

---
+++ Fabien/header.php
@@ -0,0 +1,16 @@
+<nav>
+	<div class="nav-wrapper">
+		<a href="" class="brand-logo right">Test Riasec</a>
+		<a href="" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+		<ul id="nav-mobile" class="left hide-on-med-and-down">
+			<li><a class="waves-effect waves-light btn" href="test.php">Faire le test</a></li>
+			<li><a class="waves-effect waves-light btn" href="">Reprendre le test</a></li>
+			<li><a class="waves-effect waves-light btn" href="">Administrateur</a></li>
+		</ul>
+		<ul class="side-nav" id="mobile-demo">
+			<li><a class="waves-effect waves-light btn" href="">Faire le test</a></li>
+			<li><a class="waves-effect waves-light btn" href="">Reprendre le test</a></li>
+			<li><a class="waves-effect waves-light btn" href="">Administrateur</a></li>
+		</ul>
+	</div>
+</nav>
